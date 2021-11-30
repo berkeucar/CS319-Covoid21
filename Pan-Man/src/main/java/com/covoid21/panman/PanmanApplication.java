d0bf660395b80e5e306441b863366011d6fb814e
Kutay Demiray <kutay.demiray@ug.bilkent.edu.tr>
2021-11-30 10:27:35 +0300

    Created new project with Amazon Corretto 11 JDK

---
+++ Pan-Man/src/main/java/com/covoid21/panman/PanmanApplication.java
@@ -0,0 +1,14 @@
+package com.covoid21.panman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanmanApplication.class, args);
+        System.out.println(31);
+    }
+
+}
---
+++ Pan-Man/src/test/java/com/covoid21/panman/PanmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covoid21.panman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
