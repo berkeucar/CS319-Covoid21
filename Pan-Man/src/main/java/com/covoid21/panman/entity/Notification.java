c12c1a653cfbb7ad592d52bae63a4c595875a87f
Yağız Yaşar <yagizyasarr9@gmail.com>
2021-12-22 20:57:02 +0300

    Update Notification.java

--- Pan-Man/src/main/java/com/covoid21/panman/entity/Notification.java
+++ Pan-Man/src/main/java/com/covoid21/panman/entity/Notification.java
@@ -1,19 +1,33 @@
 package com.covoid21.panman.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.util.Date;
 
-public class Notification
-{
-    private final int id;
+@Entity
+@Table( name = "notifications" )
+@Getter
+@Setter
+public class Notification {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO )
+    //@Column(name = "id", nullable = false, updatable = false, unique = true)
+    private Long id;
+
+
     private Date date;
     private NotificationType notificationType;
-    private int receiverID;
+    private Long receiverID;
     private String message;
     private boolean isRead;
+
+    protected Notification() {}
     
-    public Notification(int id, Date date, NotificationType notificationType, int receiverID, String message, boolean isRead)
+    public Notification(Date date, NotificationType notificationType, Long receiverID, String message, boolean isRead)
     {
-        this.id = id;
         this.date = date;
         this.notificationType = notificationType;
         this.receiverID = receiverID;
