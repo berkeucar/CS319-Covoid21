37f127bdb539bb8f9b823cde9811309b0ca0ab72
Yağız Yaşar <yagizyasarr9@gmail.com>
2021-12-23 15:46:48 +0300

    Confirmation Token

---
+++ Pan-Man/src/main/java/com/covoid21/panman/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,39 @@
+package com.covoid21.panman.registration.token;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    /**
+     *
+     * @param confirmationToken
+     */
+    public void saveConfirmationToken(ConfirmationToken confirmationToken) {
+        confirmationTokenRepository.save(confirmationToken);
+    }
+
+    /**
+     *
+     * @param token
+     * @return
+     */
+    public Optional<ConfirmationToken> getToken(String token) {
+        return confirmationTokenRepository.findByToken(token);
+    }
+
+    /**
+     *
+     * @param token
+     * @return
+     */
+    public int setConfirmedAt(String token) {
+        return confirmationTokenRepository.updateConfirmedAt(token, LocalDateTime.now());
+    }
+}
