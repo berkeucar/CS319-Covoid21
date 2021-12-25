5e343dc93e731cea876de5187621c5e0ee2ad659
Yağız Yaşar <yagizyasarr9@gmail.com>
2021-12-25 17:37:29 +0300

    ConfirtmationTokenServiceBase

--- Pan-Man/src/main/java/com/covoid21/panman/registration/RegistrationService.java
+++ Pan-Man/src/main/java/com/covoid21/panman/registration/RegistrationService.java
@@ -37,9 +37,6 @@ public class RegistrationService {
                     )
             );
 
-
-
-
         String link = "http://localhost:8080/api/v1/registration/confirm?token=" + token;
         emailSender.send(request.getEmail(), buildEmail(request.getUniversityID(), link));
 
